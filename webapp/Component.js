362a30712db4c370bf80a4b26ab9e3ecb4860478
MADT33 <martinditoro@gmail.com>
2022-03-31 13:28:47 +0000

    Primer commit Cap proyect

---
+++ webapp/Component.js
@@ -0,0 +1,1 @@
+jQuery.sap.declare("vital.ZCOMERCIALIZACION.Component");sap.ui.getCore().loadLibrary("sap.ui.generic.app");jQuery.sap.require("sap.ui.generic.app.AppComponent");sap.ui.generic.app.AppComponent.extend("vital.ZCOMERCIALIZACION.Component",{metadata:{manifest:"json"}});
